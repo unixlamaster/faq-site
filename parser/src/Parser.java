6d08cbacc5ff61fb80b296f7ef54639c53e1dd24
unixlamaster <unixlamaster@mail.ru>
2019-10-13 18:32:31 +0000

    change parser

--- parser/src/Parser.java
+++ parser/src/Parser.java
@@ -4,40 +4,56 @@ import java.io.*;
 import java.net.URL;
 import faq.ReadConfig;
 import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.Collection;
+import com.mongodb.*;
     
 public class Parser {
+    static String find_tag(TagNode html, String xpath) {
+        try {
+            Object[] header = html.evaluateXPath(xpath);
+            if (header != null && header.length > 0) {                        
+                return header[0].toString().trim();
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }        
+    }
+   public static BasicDBObject page_parser(HtmlCleaner htmlCleaner, Map<String, Object> config) {
+      BasicDBObject body = new BasicDBObject();
+      try {
+          TagNode html = htmlCleaner.clean(new URL((String)config.get("page_url")));     
+          Map<String, Object> fields = (Map<String, Object>)config.get("fields");
+          for (Map.Entry<String, Object> pair: fields.entrySet()) {
+             System.out.println("key="+pair.getKey());
+             System.out.println("xpath="+pair.getValue());
+             String tag_value = find_tag(html, (String)pair.getValue());
+             System.out.println(pair.getKey()+"=>"+tag_value);
+          }
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      return null; 
+  }
   public static void main(String[] args) throws Exception {
-    System.out.println("!!!");
+    System.out.println(String.join(", ", args));
     final CleanerProperties props = new CleanerProperties();
     final HtmlCleaner htmlCleaner = new HtmlCleaner(props);
     final SimpleHtmlSerializer htmlSerializer = 
         new SimpleHtmlSerializer(props);
-    Map<String, String> config = ReadConfig.map("config.yaml");
-    for (Map.Entry<String, String> pair: config.entrySet())
-            {
-                System.out.println(pair.getKey()+"=>"+pair.getValue());
-            }     
+    Map<String, Object> config = ReadConfig.map("config.yaml");
+    for (Map.Entry<String, Object> pair: config.entrySet()) {
+         System.out.println(pair.getKey()+"=>"+pair.getValue());
+    }     
     // make 10 threads using the same cleaner and the same serializer 
 
     for (int i = 1; i <= 1; i++) {
-        final String url = "https://otvet.mail.ru/question/216156098";
-        final String fileName = "c:/temp/ebay_art" + i + ".xml";
         Thread t = new Thread(new Runnable() {
-
             public void run() {
-                try {
-                    TagNode html = htmlCleaner.clean(new URL(url));
-                    Object[] header = html.evaluateXPath(config.get("q_header"));
-                    System.out.print("header\t: ");
-                    if (header != null && header.length > 0) {                        
-                        System.out.println(header[0].toString().trim());
-                    } else {
-                        System.out.println("not found");
-                    }
-    //                htmlSerializer.writeToFile(tagNode, fileName, "utf-8");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                BasicDBObject body = page_parser(htmlCleaner, config);
             }
         });
         t.start();
--- parser/src/ReadConfig.java
+++ parser/src/ReadConfig.java
@@ -9,10 +9,10 @@ import java.util.Map;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 public class ReadConfig {
-  private static Map<String, String> readYamlConfig(String yaml) {
+  private static Map<String, Object> readYamlConfig(String yaml) {
     try {
       ObjectMapper yamlReader = new ObjectMapper(new YAMLFactory());
-      Map<String, String> obj = yamlReader.readValue(yaml, new TypeReference<Map<String,String>>(){});
+      Map<String, Object> obj = yamlReader.readValue(yaml, new TypeReference<Map<String,Object>>(){});
       return obj;
     } catch(Exception ioe)
       {
@@ -21,11 +21,11 @@ public class ReadConfig {
       }
   }
   
-  public static Map<String, String> map(String config_file_name){
+  public static Map<String, Object> map(String config_file_name){
     try
     {
         String content = Files.readString(Paths.get(config_file_name));
-        Map<String, String> config = readYamlConfig(content);
+        Map<String, Object> config = readYamlConfig(content);
         return config;
      } catch(Exception ioe)
       {
